b1464502d90e05b14edfaa860af314172d7260ce
suhas <suhas@live.ca>
2025-08-24 03:24:30 -0400

    fix: domain name

--- app/root.tsx
+++ app/root.tsx
@@ -44,7 +44,7 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  { rel: "canonical", href: "https:// ilovesvg.com" },
+  { rel: "canonical", href: "https://ilovesvg.com" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
