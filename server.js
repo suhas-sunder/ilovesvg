6955e787db841b410a166ef1567f355dc489c2ed
Suhas Sunder <suhas@live.ca>
2026-01-30 18:49:04 -0500

    fix: server port

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ if (DEVELOPMENT) {
   app.use(await import(BUILD_PATH).then((mod) => mod.app));
 }
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
